b4681696671317f95e8cafb720461a795c0a0950
Kprjjl <69335092+Kprjjl@users.noreply.github.com>
2024-02-17 16:07:45 +0800

    Imported axios and Updated Users.js

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,11 +1,20 @@
 import { Link, useParams } from "react-router-dom";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function Users() {
     const { id } = useParams()
 
     const [data, setData] = useState([])
-    
+
+    useEffect(() => {
+        axios.get('http://localhost:3001/')
+            .then(res => {
+                console.log(res);
+            })
+            .catch(err => console.log(err));
+    }, [])
+
     return (
         <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
             <div className="w-50 bg-white rounded p-3">
@@ -26,3 +35,5 @@ function Users() {
         </div>
     );
 }
+
+export default Users;
